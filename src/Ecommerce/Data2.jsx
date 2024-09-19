95507225daac9b585a9859ed03481a65d9e0e235
Dhruvsinghdk <re621006@gmail.com>
2024-09-19 12:39:17 +0530

    new

--- src/Ecommerce/Data2.jsx
+++ src/Ecommerce/Data2.jsx
@@ -20,83 +20,83 @@ const Data2 = [
     {
         id:"A movie or film is a type of visual art that uses images and sounds to tell stories or teach people something. Most people watch movies to entertain themselves or to have fun",
         img:img1,
-        Nm:"School"
+        Nm:<div style={{color:"grey",textDecoration:"underline"}}>School</div>
     },
     {
         id:"Most movies are made so they can be watched at home or on a movie screen at movie theaters. Movies are shown in movie theaters a few weeks or months after or before the movie is released",
         img:img2,
-        Nm:"Now You see"
+        Nm:<div style={{color:"greenyellow",textDecoration:"underline"}}>Now You See Me</div>
     },
     {
         id:"Movies are filmed with movie cameras or video cameras. Movie cameras take pictures very quickly, and they usually take 24 or 25 pictures, or frames every second.",
         img:img3,
-        Nm:"Korean"
+        Nm:<div style={{color:"ActiveBorder",textDecoration:"underline"}}>Korean</div>
     },
     {
         id:"Most movies ts th used to pay for the d the equipment.  by borrowing it from a bank or by a give money to them. Some producers work for a movie studio, but other producers work by themselves.",
         img:img7,
-        Nm:"Step Up"
+        Nm:<div style={{color:"aqua",textDecoration:"underline"}}>Step Up</div>
     },
     {
         id:"A genre means a type of movie. Movies can be fictional, or non-fictional. Even though hundreds of movies are made every year, there are not like another movie. Some movies can be two or more genres.",
         img:img8,
-        Nm:"Hunger Games"
+        Nm:<div style={{textDecoration:"underline"}}>Hunger Games</div>
     },
     ,
     {
         id:"The industry has always been led by a few major movie studios like MGM/UA, Warner Bros., Columbia, Paramount, or Disney. The oldest American movie studio is Universal.",
         img:img9,
-        Nm:"Fist"
+        Nm:<div style={{color:"pink",textDecoration:"underline"}}>Fist</div>
     },
     {
         id:"There are many large companies t t effects, lighting, set building. Many of these employees belong to trade unions who say how much their members must be paid. A large number of smaller companies also offer services, such as music studios",
         img:img10,
-        Nm:"Kung Fu"
+        Nm:<div style={{color:"purple",textDecoration:"underline"}}>Kung Fu</div>
     },
     {
         id:"In blockbuster movit (story) are figured out or fixed and almost everyone (except the bad guys) live happily ever after. Some movies have been so successful that the studios keep releasing more and more sequels, or movies with the same characters and basic plots.",
         img:img11,
-        Nm:"Dead Or Alive"
+        Nm:<div style={{color:"Green"}}>Dead Or Alive</div>
     },
     {
         id:"In blockblmost ehappily ever after. Some movies have been so successful that the studios keep releasing more and more sequels, or movies with the same characters and basic plots.",
         img:img12,
-        Nm:"Dead Or Alive"
+        Nm:<div style={{color:"blue",textDecoration:"underline"}}>Master Ip</div>
     },
     {
         id:"In blockblmost ehappily ever after. Some movies have been so successful that the studios keep releasing more and more sequels, or movies with the same characters and basic plots.",
         img:img13,
-        Nm:"Dead Or Alive"
+        Nm:<div style={{color:"blue",textDecoration:"underline"}}>Swildlers</div>
     },
     {
         id:"In blockblmost ehappily ever after. Some movies have been so successful that the studios keep releasing more and more sequels, or movies with the same characters and basic plots.",
         img:img13,
-        Nm:"Dead Or Alive"
+        Nm:<div style={{color:"blue",textDecoration:"underline"}}>Swindlers Part 2</div>
     },
     {
         id:"In blockblmost ehappily ever after. Some movies have been so successful that the studios keep releasing more and more sequels, or movies with the same characters and basic plots.",
         img:img14,
-        Nm:"Dead Or Alive"
+        Nm:<div style={{color:"blue",textDecoration:"underline"}}>Lucky Key</div>
     },
     {
         id:"In blockblmost ehappily ever after. Some movies have been so successful that the studios keep releasing more and more sequels, or movies with the same characters and basic plots.",
         img:img15,
-        Nm:"Dead Or Alive"
+        Nm:<div style={{color:"blue",textDecoration:"underline"}}>Good Guy</div>
     },
     {
         id:"In blockblmost ehappily ever after. Some movies have been so successful that the studios keep releasing more and more sequels, or movies with the same characters and basic plots.",
         img:img16,
-        Nm:"Dead Or Alive"
+        Nm:<div style={{color:"blue",textDecoration:"underline"}}>Steal My Heart</div>
     },
     {
         id:"In blockblmost ehappily ever after. Some movies have been so successful that the studios keep releasing more and more sequels, or movies with the same characters and basic plots.",
         img:img17,
-        Nm:"Dead Or Alive"
+        Nm:<div style={{color:"blue",textDecoration:"underline"}}>Hi Venus</div>
     },
     {
         id:"In blockblmost ehappily ever after. Some movies have been so successful that the studios keep releasing more and more sequels, or movies with the same characters and basic plots.",
         img:img18,
-        Nm:"Dead Or Alive"
+        Nm:<div style={{color:"blue",textDecoration:"underline"}}>Still 17</div>
     },
 
 ]
